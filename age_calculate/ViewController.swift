bd0800840327100f788efc5078bb3d308ff4b9ef
KienNguyen2021 <kien.computersciene@gmail.com>
2024-11-22 09:33:55 -0700

    first commit

--- age_calculate/ViewController.swift
+++ age_calculate/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userinput: UITextField!
+    
+    
+    @IBAction func calculateButton(_ sender: Any) 
+    
+    {
+        
+        if let result = Int(userinput.text!)
+        {
+            ResultLabel.text = String(Int(userinput.text!)! * 8 )
+        }
+        
+        ResultLabel.text = (" Please enter a number ! " )
+    }
+    
+    
+    @IBOutlet var ResultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
