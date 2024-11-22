59d3c94ebc935eee3d7ef5273b8e905ef8607be3
KienNguyen2021 <kien.computersciene@gmail.com>
2024-11-22 07:26:55 -0700

    Initial Commit

---
+++ age_calculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  age_calculate
+//
+//  Created by Kien Nguyen on 2024-11-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
